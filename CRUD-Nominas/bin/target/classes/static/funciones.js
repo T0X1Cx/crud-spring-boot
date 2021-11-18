2e3bec2ad3c929cc765324bb539e50636982d4da
T0X1Cx <71453093+T0X1Cx@users.noreply.github.com>
2021-11-18 12:19:46 +0100

    Add files via upload

---
+++ CRUD-Nominas/bin/target/classes/static/funciones.js
@@ -0,0 +1,32 @@
+function eliminarAlert(){
+	
+swal({
+  title: "¡Buen trabajo!",
+  text: "Empleado eliminado satisfactoriamente",
+  icon: "success",
+  button: "Aceptar",
+},2000);
+	
+}
+
+function crearAlert(){
+	
+	swal({
+  title: "¡Buen trabajo!",
+  text: "Empleado creado satisfactoriamente",
+  icon: "success",
+  button: "Aceptar",
+});
+	
+}
+
+function actualizarAlert(){
+	
+	swal({
+  title: "¡Buen trabajo!",
+  text: "Empleado actualizado satisfactoriamente",
+  icon: "success",
+  button: "Aceptar",
+});
+	
+}
